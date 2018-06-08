ecb4ac637a81cc9e30f74d6971e50a5246abc199
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-08 17:06:38 +0300

    ++

---
+++ TestDelegate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestDelegate
+{
+    delegate int P(string s);
+
+    class Program
+    {
+
+
+        void M(string s)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            // 1 вариант
+            //string s;
+            //M((s) => { Console.WriteLine(s); return 0; }, "prog-school.ru");
+
+            //2
+            int A(string str) { Console.WriteLine(str); return str.Length; }
+            M(A, "GeekBrains.ru");
+
+            //3
+            //M(delegate (string s) { Console.WriteLine(s); return 0; }, "GeekBrains.ru");
+
+            //4
+            //M((string)=>{ Console.WriteLine(string); return 0; },"GeekBrains.ru");
+
+            //5
+
+            //Func D = new Func(A);
+            //M(D, "GeekBrains");
+        }
+
+        int A(string str) { Console.WriteLine(str); return str.Length; }
+        //void M(P, string str )
+        //{
+
+        //}
+
+
+    }
+}
--- TestOOP/Program.cs
+++ TestOOP/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace TestOOP
 {
+    class M
+    {
+        public  void A()
+        {
+            Console.Write("M-A ");
+        }
+
+        virtual public void B()
+        {
+            Console.Write("M-B ");
+        }
+        public  void    C()
+        {
+            Console.Write("M-C ");
+        }
+    }
+
+    class N : M
+    {
+        public void A()
+        {
+            Console.Write("N-A ");
+        }
+
+        public override void B()
+        {
+            Console.Write("N-B ");
+        }
+        
+        public  void    C()
+        {
+            Console.Write("N-C ");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            M m = new M();
+            N n = new N();
+            M mn = new N();
+
+            m.A();
+            n.C();
+            mn.C();
+            mn.B();
+            Console.ReadKey();
         }
     }
 }
